2fab6092c01d6ef35ca58349ca9ec85970095816
Michael Harrison <harrison464@gmail.com>
2025-11-15 16:19:17 -0500

    Initial Commit

---
+++ SugarBuddy/SugarBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarBuddyApp.swift
+//  SugarBuddy
+//
+//  Created by Michael Harrison on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SugarBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
